08fe40364ccfd827f6fc7b02863ef9471f043822
Shisha1536 <135944742+Shisha1536@users.noreply.github.com>
2023-08-21 03:00:26 +0300

    js*

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const image = Array.prototype.slice.call(document.getElementsByTagName("img"));
-        
-let list = document.getElementById('points');
-image.forEach((element) => {
-    let item = document.createElement('a');
-    item.href = '#' + element.id;
-    item.innerHTML = "•";
-    list.append(item);
-});
